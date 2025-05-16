206261c81e16bdb571d914918a2679e02220eb1b
minhtrinhthao0409 <minhtrinh.33241024113@st.ueh.edu.vn>
2025-05-16 14:07:03 +0700

    Models

---
+++ Phan2/Models/Bank_account.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phan2.Models
+{
+    public enum Type
+    {
+        Saving = 0,
+        Checking = 1
+
+    }
+
+    public class Bank_account
+    {
+        public BigInteger Account_id { get; set; }
+        public string Owner_name { get; set; }
+        public string Owner_phone { get; set; }
+        public double Balance { get; set; } = 0;
+        public Type Account_type { get; set; } = 0;
+        public string Password { get; set; } 
+
+        public Bank_account() { }
+        
+
+    }
+}
---
+++ Phan2/Models/Transactions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phan2.Models
+{
+    public class Transactions
+    {
+        public Guid Trans_id { get; set; } = Guid.NewGuid();
+        public BigInteger Account_id { get; set; }
+
+        public DateTime Happened_time { get; set; } = DateTime.UtcNow;
+        public string Action_desc { get; set; }
+        public string Note { get; set; } = null!;
+    }
+}
